98fcd525cb4252085ec2b6fd74999d179e79f1d1
estebanjb1989 <69223801+estebanjb1989@users.noreply.github.com>
2024-07-05 11:21:13 -0300

    adjusted styles

--- src/styles/index.ts
+++ src/styles/index.ts
@@ -1,17 +1,7 @@
 import styled from "styled-components/native";
 
-export const ButtonContainer = styled.View`
-  align-items: flex-end;  
-  margin-top: 18px;
-  width: 240px;
-`;
-
-export const Filler = styled.View<{ 
-  height?: number;
-}>`
-  height: ${({ height }) => height ? `${height}px` : "0px"};
-`
-
+// Content spacing 
+// column
 export const Group = styled.View<{ 
   contained?: boolean; 
   centered?: boolean;
@@ -20,10 +10,24 @@ export const Group = styled.View<{
   justify-content: center;
   align-items: ${({ centered }) => centered ? "center" : "flex-start"};
   background-color: ${({ contained }) => contained ? "#DEDFE4" : "transparent"};
-  padding: ${({ contained }) => contained ? "24px" : "0px"};
+  padding-vertical: ${({ contained }) => contained ? "24px" : "0px"};
+  padding-horizontal: ${({ contained }) => contained ? "16px" : "0px"};
   border-radius: ${({ contained }) => contained ? "8px" : "0px"};
 `;
 
+// row
+export const Row = styled.View`
+  flex-direction: row;
+  align-items: center;
+`
+// Fixing default group button container
+export const ButtonContainer = styled.View`
+  align-items: flex-end;  
+  width: 240px;
+  margin-top: 16px;
+`;
+
+// Components for app title/banner/logo, subsection title, 
 export const AppTitle = styled.Text`
   font-size: 30px;
 `;
@@ -33,7 +37,9 @@ export const Title = styled.Text`
   margin-bottom: 8px;
 `;
 
-export const Row = styled.View`
-  flex-direction: row;
-  align-items: center;
+// height filler
+export const Filler = styled.View<{ 
+  height?: number;
+}>`
+  height: ${({ height }) => height ? `${height}px` : "0px"};
 `
